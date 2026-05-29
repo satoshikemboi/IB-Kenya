c6314dfc4349f64ff11e2a9858cdb0c4d1900049
satoshikemboi <satoshikemboi@gmail.com>
2026-05-29 15:21:51 +0300

    removed broker finder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,6 @@ import { Link, NavLink, useLocation } from "react-router-dom";
 const navLinks = [
   { label: "Brokers", href: "/brokers" },
   { label: "Categories", href: "/brokers/category" },
-  { label: "Broker Finder", href: "/broker-finder" },
   { label: "Methodology", href: "/methodology" },
   { label: "About", href: "/about" },
 ];
